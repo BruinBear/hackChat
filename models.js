c13ae9d81cce0e18a830cc25ca0a0f78636d5e6b
Steven Collison <stevendonv@gmail.com>
2014-04-12 20:14:40 -0400

    changes collection model to include a single active user collection. makes find_match more powerful. adapts the Meteor.methods to accomodate the changes

--- models.js
+++ models.js
@@ -5,7 +5,4 @@ Messages = new Meteor.Collection('messages');
 
 Channels = new Meteor.Collection('channels');
 
-Active_Teachers = new Meteor.Collection('teachers');
-Active_Students = new Meteor.Collection('students');
-
-
+Active_Users = new Meteor.Collection('active-users');
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 Meteor.startup(function () {
 	Channels.insert({id : 0});
-};
+});
 
 Meteor.methods({
 	
@@ -10,44 +10,72 @@ Meteor.methods({
 		},
 
 	//finds a teacher match for the student based on their subjects
-	//returns the teacher's id or -1 for no match
+	//if a match is found, place the teacher and student in a new channel
 	findMatch : function() {
-		var student = Active_Students.find({id : this.id});
-		if (user.count != 1) {
-			return -1; 
-		}
+	
+		//get the caller's record
+		var caller = Active_Users.findOne({id : this.id}); 
+		
+		//if the caller was a student try to find a suitable teacher
+		//otherwise find a suitable student 
+		if (caller.role == "student")
+			var other_id = Active_Users.findOne({role : "teacher"},{available : true},
+						{cid : 0},{subject : caller.subject});
 
-		var teachers = Active_Teachers.findOne({subjects : {$in : student.subjects},
-						      {available : true});
-	
-		if (teachers.count == 1) 
-			return teachers.id;
 		else 
+			var other_id = Active_Users.findOne({role : "student"},{available : true},
+						{cid : 0});  
+		
+		if (other_id.count != 1)
 			return -1; 
-	},
-	
-	//create a new channel(channelId is returned)   
-	createChannel : function(type,name) {
+		
+		//create an empty channel
 		var channels = Channels.find({});
 		var max_cid = channels.count;
 		
 		Channels.insert({ id : max_cid+1, 
-				  name : name 
-				  type : type
+				  name : "Private Help Session", 
+				  type : "private",
+				  num_users : 2});
+
+		 //add matched users to the channel
+		 Active_Users.update({id : this.id},{$set : {cid : max_cid}});
+		 Active_Users.update({id : other_id},{$set : {cid : max_cid}}); 
+
+		//return channel id
+		return max_cid;  
+	},
+	
+	//create a new channel(channelId is returned)   
+	createChannel : function(ctype,cname) {
+		var channels = Channels.find({});
+		var max_cid = channels.count;
+		
+		Channels.insert({ id : max_cid+1, 
+				  name : cname, 
+				  type : ctype,
 				  num_users : 0});  
 		return max_cid; 
 	},
 	
 	//returns the number of channels that were updated(should be 1)
-	joinChannel : function(cid) {
+	joinChannel : function(chanid) {
+
+		//update the user's channel id 
+		Active_Users.update({id : this.id},{$set : {cid : chanid}});
 		
-		return Channels.update({id : cid},{$inc : {num_users : 1}});
+		//update channel count 
+		return Channels.update({id : chanid},{$inc : {num_users : 1}});
 	},
 
-	leaveChannel : function(cid) {	
-		Channels.update({id : cid},{$inc : {num_users : -1}});
-		if (Channels.find({id : cid}).num_users == 0)
-			Channels.remove({id : cid});
+	leaveChannel : function(chanid) {
+		//change the user's channel id to 0(default channel) 
+		Active_Users.update({id : this.id},{$set : {cid : 0}});
+		
+		//update channel counts
+		Channels.update({id : chanid},{$inc : {num_users : -1}});
+		if (Channels.find({id : chanid}).num_users == 0 && (chanid != 0)
+			Channels.remove({id : chanid});
 	}
 
 });  
